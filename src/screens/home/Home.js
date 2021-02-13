50c9c555ea63ef9d24d9863c91377b625ffdb367
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2021-02-13 06:55:41 +0530

    Added 'Genre' filter for movies on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
 import moviesData from '../../common/movieData';
+import genres from '../../common/genre';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -12,6 +13,10 @@ import FormControl from '@material-ui/core/FormControl';
 import Typography from '@material-ui/core/Typography';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const styles = theme => ({
     root: {
@@ -47,12 +52,17 @@ const styles = theme => ({
      constructor() {
          super();
          this.state = {
-             movieName: ""
+             movieName: "",
+             genres: []
          }
      }
 
     movieNameChangeHandler = (e) => {
         this.setState({movieName: e.target.value});
+    }
+
+    genreSelectHandler = (e) => {
+        this.setState({genres: e.target.value});
     }
 
     render() {
@@ -99,6 +109,23 @@ const styles = theme => ({
                                 <FormControl className={classes.formControl}>
                                     <InputLabel htmlFor="movieName"> Movie Name </InputLabel>
                                     <Input id="movieName" onChange={this.movieNameChangeHandler} />
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="select-multiple-checkbox"> Genre </InputLabel>
+                                <Select
+                                    multiple
+                                    input={<Input id="select-multiple-checkbox" />}
+                                    renderValue={selected => selected.join(',')}
+                                    value={this.state.genres}
+                                    onChange={this.genreSelectHandler}>
+                                <MenuItem value="0">None</MenuItem>
+                                {genres.map(genre => (
+                                    <MenuItem key={genre.id} value={genre.name} >
+                                        <Checkbox checked={this.state.genres.indexOf(genre.name) > -1} />
+                                        <ListItemText primary={genre.name} />
+                                    </MenuItem> 
+                                ))}
+                                </Select>
                                 </FormControl>
                             </CardContent>
                         </Card>
