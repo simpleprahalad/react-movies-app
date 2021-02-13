cc13edc7411d0051a67cdbee0b707675c7fa2730
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2021-02-13 17:36:34 +0530

    Added rating on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -9,12 +9,40 @@ import YouTube from 'react-youtube';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StarBorderIcon from '@material-ui/icons/StarBorder'
 
 class Details extends Component {
     constructor() {
         super();
         this.state = {
-            movie: {}
+            movie: {},
+            starIcons: [
+                {
+                   id: 1,
+                   stateId: "star1",
+                   color: "black"
+                },
+                {
+                   id: 2,
+                   stateId: "star2",
+                   color: "black"
+                },
+                {
+                   id: 3,
+                   stateId: "star3",
+                   color: "black"
+                },
+                {
+                   id: 4,
+                   stateId: "star4",
+                   color: "black"
+                },
+                {
+                   id: 5,
+                   stateId: "star5",
+                   color: "black"
+                }
+             ]
         }
     }
 
@@ -33,6 +61,22 @@ class Details extends Component {
 
     artistClickHandler = (url) => {
         window.location = url;
+    }
+
+    starClickHandler = (id) => {
+        let starIconList = [];
+        for (let star of this.state.starIcons) {
+            let starNode = star;
+            if (star.id <= id) {
+                starNode.color = "yellow"
+            }
+            else {
+                starNode.color = "black";
+
+            }
+            starIconList.push(starNode);
+        }
+        this.setState({ starIcons: starIconList });
     }
 
     render() {
@@ -94,6 +138,16 @@ class Details extends Component {
                     </div>
 
                     <div className="rightDetails">
+                    <Typography>
+                        <span className="bold">Rate this movie: </span>
+                        {this.state.starIcons.map(star => (
+                            <StarBorderIcon
+                                className={star.color}
+                                key={"star" + star.id}
+                                onClick={() => this.starClickHandler(star.id)}
+                            />
+                        ))}
+                    </Typography>
                     <div className="bold marginBottom16 marginTop16">
                             <Typography>
                                 <span className="bold">Artists:</span>
